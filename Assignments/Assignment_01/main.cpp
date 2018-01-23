2fc3747a086460ff7591eac8c9ceddc237dc0237
mattjmoses <mattjmoses@gmai.com>
2018-01-23 09:44:39 -0400

    Setting up the bloody text in functionality.

--- Assignments/Assignment_01/main.cpp
+++ Assignments/Assignment_01/main.cpp
@@ -6,34 +6,34 @@ using namespace std;
 
 int main(int argc,char* argv[]) {
 
-    ofstream out(argv[1]);
-    out<<argv[1];
-    cin;
+    //Linked list object
+    LinkedList linkedList;
+    //storing text in a variable
+    string textLine;
+    //Testing out our text file
+    ifstream file("test.txt");
+    if(file.is_open())
+    {
+        while(getline(file,textLine))
+        {
+            cout << textLine << endl;
+        }
+    }
 
 
-    LinkedList newList;
-
-    newList.CreateNode("0");
-    newList.CreateNode("1");
-    newList.CreateNode("2");
-    newList.CreateNode("3");
-    newList.CreateNode("4");
-    newList.CreateNode("5");
-    newList.ReadNodes();
-    cout<<"++++++++++++++++"<<endl;
-//    newList.InsertNodeAtPosition(5,"I've inserted a new node");
-//    newList.InsertNodeAtPosition(6,"One more at the end");
-//    newList.InsertNodeAtPosition(0,"And a last one at the start");
 
 
-//    newList.ReadNodes();
-    //newList.DeleteNode(5);
-    newList.UpdateNode(7,"Penisman");
 
 
-    //newList.DeleteRange(2,4);
 
-    newList.ReadNodes();
+
+
+
+
+
+
+
+
 
 
     return 0;
