825285d371f4221a9bbced44f6afd034ac402e91
matt <mattjmoses@gmail.com>
2018-02-11 17:59:10 -0400

    Seem to be able to pass the text into the array now...

--- Assignments/Assignment_02/main.cpp
+++ Assignments/Assignment_02/main.cpp
@@ -18,6 +18,7 @@ int main() {
     {
         while(getline(readFile,textLine))
         {
+
             //counting the rows
             rowcount ++;
             //getting the max num of columns
@@ -27,34 +28,49 @@ int main() {
             }
         }
     }
+    readFile.close();
     cout << "Row count: " << rowcount << endl;
     cout << "Columns: " << colCount << endl;
+
     //New empty dynamic array.
     char **mazeArray;
-
     mazeArray = new char *[rowcount];
+    //Creating all the columns from the maze data
     for (int i = 0; i < rowcount; i++)
     {
         mazeArray[i] = new char[colCount];
     }
-//
-    //Setting the values for the  2D array.
-    for(int i = 0; i < rowcount; i++)
+
+    ifstream mazeFile("maze.txt");
+    string mazeText;
+    int row = -1;
+    //I think this works...
+    if(mazeFile.is_open())
     {
-        for(int j = 0; j < colCount; j++)
+        while(getline(mazeFile,mazeText))
         {
-            mazeArray[i][j] = '+';
+            row ++;
+            cout << mazeText << endl;
+
+            for(int j = 0; j < colCount; j++)
+            {
+                mazeArray[row][j] = mazeText[j];
+            }
         }
     }
+
+//    //Setting the values for the  2D array.
+//    string text;
+//    for(int i = 0; i < rowcount; i++)
+//    {
 //
-    for(int i = 0; i < rowcount; i++)
-    {
-        for(int j = 0; j < colCount; j++)
-        {
-            cout << mazeArray[i][j];
-        }
-        cout << endl;
-    }
+//        for(int j = 0; j < colCount; j++)
+//        {
+//            mazeArray[i][j] = textLine[j];
+//        }
+//    }
+
+
 
 
     //We don't even need to goddamn convert the string to a char array. It plays just fine! HUH
