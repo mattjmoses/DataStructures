2c64cab0869735cdef671bf25ee0cefacba46186
matt <mattjmoses@gmail.com>
2018-02-11 17:21:18 -0400

    Generating the 2D array based on the maze dimensions. DONE Next step is getting the GD maze into the array.

--- Assignments/Assignment_02/main.cpp
+++ Assignments/Assignment_02/main.cpp
@@ -6,47 +6,55 @@
 using namespace std;
 
 int main() {
-//    ifstream readFile("maze.txt");
-//    string textLine;
-//    int rowcount = -1;
-//    //We got the dimensions of the maze.
-//    //Next we need to read the maze in line by line and pull the string into a bunch of chars...
-//    if(readFile.is_open())
-//    {
-//        while(getline(readFile,textLine))
-//        {
-//            rowcount ++;
-//            cout << "Rows in Maze"<< rowcount<< endl;
-//            cout << textLine.length() << endl;
-//        }
-//    }
-//    //New empty dynamic array.
-//    char **newArray;
-//    int rows = 9;
-//    int col = 40;
-//    newArray = new char *[rows];
-//    for (int i = 0; i < rows; i++)
-//    {
-//        newArray[i] = new char[col];
-//    }
+    //Text file for maze
+    ifstream readFile("maze.txt");
+    string textLine;
+    //Counting the rows
+    int rowcount = -1;
+    //Counting the columns
+    int colCount = 0;
+    //We get the dimensions of the maze by scanning the text file.
+    if(readFile.is_open())
+    {
+        while(getline(readFile,textLine))
+        {
+            //counting the rows
+            rowcount ++;
+            //getting the max num of columns
+            if(textLine.length() > colCount)
+            {
+                colCount = textLine.length();
+            }
+        }
+    }
+    cout << "Row count: " << rowcount << endl;
+    cout << "Columns: " << colCount << endl;
+    //New empty dynamic array.
+    char **mazeArray;
+
+    mazeArray = new char *[rowcount];
+    for (int i = 0; i < rowcount; i++)
+    {
+        mazeArray[i] = new char[colCount];
+    }
 //
-//    //Setting the values for the  2D array.
-//    for(int i = 0; i < rows; i++)
-//    {
-//        for(int j = 0; j < col; j++)
-//        {
-//            newArray[i][j] = '+';
-//        }
-//    }
+    //Setting the values for the  2D array.
+    for(int i = 0; i < rowcount; i++)
+    {
+        for(int j = 0; j < colCount; j++)
+        {
+            mazeArray[i][j] = '+';
+        }
+    }
 //
-//    for(int i = 0; i < rows; i++)
-//    {
-//        for(int j = 0; j < col; j++)
-//        {
-//            cout << newArray[i][j];
-//        }
-//        cout << endl;
-//    }
+    for(int i = 0; i < rowcount; i++)
+    {
+        for(int j = 0; j < colCount; j++)
+        {
+            cout << mazeArray[i][j];
+        }
+        cout << endl;
+    }
 
 
     //We don't even need to goddamn convert the string to a char array. It plays just fine! HUH
