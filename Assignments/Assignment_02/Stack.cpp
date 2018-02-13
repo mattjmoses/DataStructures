37bd5d88a209df6963b00c52a9ff719553c91d90
matt <mattjmoses@gmail.com>
2018-02-12 21:35:05 -0400

    There's a stack build. Now to test the damn thing.

--- Assignments/Assignment_02/Stack.cpp
+++ Assignments/Assignment_02/Stack.cpp
@@ -3,21 +3,30 @@
 //
 #include "Node.h"
 #include "Stack.h"
-
-
+#include "iostream"
+using namespace std;
+//Referencing code from http://www.cppforschool.com
+//The ol constuctor
 Stack::Stack()
 {
     top = nullptr;
-
 }
 
 Stack::~Stack()
 {
+    //Looping through and removing all the stack elements from memory
+    while(top!= nullptr)
+    {
+        Node *temp = top;
+        top = top->nodePtr;
+        delete temp;
+    }
 
 }
-
+//Push new elements onto the stack
 void Stack::push(int x, int y)
 {
+    //Creating a new node and setting the x and y values and adding it to the top.
     Node *tempNode;
     tempNode = new Node;
     tempNode->coords[0] = x;
@@ -25,11 +34,28 @@ void Stack::push(int x, int y)
     tempNode->nodePtr = top;
     top = tempNode;
 }
-
-void Stack::pop() {
+//Removing elements from the stack
+void Stack::pop()
+{
+    //If the top element isn't null then remove it and say what was removed
+    if(top != nullptr)
+    {
+        Node *temp=top;
+        top = top->nodePtr;
+        cout<< "Coordinates X: " << temp->coords[0]<<"Y: "<< temp->coords[1] << " removed from stack";
+        delete temp;
+    }
+    else
+        cout<<"Stack empty";
 
 }
-
-void Stack::display() {
-
+//Display top element of the stack
+void Stack::display()
+{
+    Node *temp = top;
+    while(temp!= nullptr)
+    {
+        cout<<"X coordinate: "temp->coords[0]<<" Y coordinate: " <<temp->coords[1] << endl;
+        temp=temp->nodePtr;
+    }
 }
