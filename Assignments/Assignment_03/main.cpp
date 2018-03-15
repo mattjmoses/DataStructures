f6cd673854f17711f2b580e32db6253676893dba
matt <mattjmoses@gmail.com>
2018-03-15 20:14:47 -0300

    Need to copy the shuffled array over I think...

--- Assignments/Assignment_03/main.cpp
+++ Assignments/Assignment_03/main.cpp
@@ -19,17 +19,18 @@ int main()
     //sorter.SelectionSort(shuffledArray,1000);
     //sorter.InsertionSort(shuffledArray,10);
     //sorter.ShellSort(shuffledArray,1000);
-    int a[] = {-100,10,43,23,-78,45,123,56,98,41,90,24};
+
+    int arrayCopy[] = {-100,10,43,23,-78,45,123,56,98,41,90,24,8,7,7,7,7,3};
     int num;
 
-    num = sizeof(a)/sizeof(int);
+    num = sizeof(arrayCopy)/sizeof(int);
 
-    int b[num];
+    int tempArray[num];
 
-    sorter.MergeSort(a,b,0,num-1);
+    sorter.MergeSort(arrayCopy,tempArray,0,num-1);
 
     for(int i=0; i<num; i++)
-        cout<<a[i]<<" ";
+        cout<<arrayCopy[i]<<",";
     cout<<endl;
 
 
