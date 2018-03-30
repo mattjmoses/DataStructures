0b17dc1271cae44793ce94568bc2f68e2cd45237
matt <mattjmoses@gmail.com>
2018-03-29 22:25:05 -0300

    Well my searches work, the methods are presently outputting incoherent numbers for my times. SO there's something.

--- Assignments/Assignment_04/main.cpp
+++ Assignments/Assignment_04/main.cpp
@@ -13,12 +13,62 @@ int main() {
         array[i] = i;
 
     }
+    int result;
+    int result02;
+    fstream seq("seqSearch.txt", ios::in | ios::out | ios::app);
 
-    int result = goSearch.sequentialSearch(array,50);
-    int result02 = goSearch.binarySearch(array, 0,arraySize,999);
-    cout << result;
-    cout << endl;
-    cout << result02;
+    if(!seq.is_open())
+    {
+        cout << "error happen";
+    }
+    else
+    {
+        for(int w =0; w < arraySize; w++)
+        {
+            //timer start
+            clock_t begin = clock();
+            result = goSearch.sequentialSearch(array,w);
+            //timer stop
+            clock_t end = clock();
+
+            //Calculating time in seconds
+            double timeSec = (end - begin) / static_cast<double>( CLOCKS_PER_SEC );
+            //writer to file
+            seq<< "Time to search for " << result <<" : " << timeSec << endl;
+        }
+        seq.close();
+    }
+
+
+
+
+
+    fstream bin("binSearch.txt", ios::in | ios::out | ios::app);
+    if(!bin.is_open())
+    {
+        cout << "error happen";
+    }
+    else
+    {
+        for(int q = 0; q < arraySize; q++)
+        {
+            //timer start
+            clock_t begin = clock();
+            result02 = goSearch.binarySearch(array, 0,arraySize,q);
+            //timer stop
+            clock_t end = clock();
+
+            //Calculating time in seconds
+            double timeSec = (end - begin) / static_cast<double>( CLOCKS_PER_SEC );
+            //write to file
+            bin<< "Time to search for " << result02 <<" : " << timeSec << endl;
+        }
+        bin.close();
+
+    }
+
+
+
 
 
     return 0;
