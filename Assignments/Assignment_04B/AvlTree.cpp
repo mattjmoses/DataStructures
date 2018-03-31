b8acda22db02fe863cdf95bb35c59a059ed4cbe5
mattmoses <mattjmoses@gmail.com>
2018-03-31 19:15:07 -0300

    Building out the goddamn methods. Then I'm gonna feed it a dictionary

--- Assignments/Assignment_04B/AvlTree.cpp
+++ Assignments/Assignment_04B/AvlTree.cpp
@@ -3,3 +3,52 @@
 //
 
 #include "AvlTree.h"
+
+AvlTree::AvlTree() = default;
+
+AvlTree::~AvlTree() = default;
+
+int AvlTree::max(int a, int b)
+{
+    //Is a larger than b? if so return a, otherwise return b
+    return (a > b)? a : b;
+}
+
+int AvlTree::getHeight(Node *node)
+{
+    //Here we check if the node is actually there
+    if(node == nullptr)
+    {
+        return 0;
+    }
+    else
+        {
+          //Return the height of the node
+          return node->height;
+        }
+
+}
+
+AvlTree::Node *AvlTree::createNode(int) {
+    return nullptr;
+}
+
+AvlTree::Node *AvlTree::rorateRight(AvlTree::Node *) {
+    return nullptr;
+}
+
+AvlTree::Node *AvlTree::rotateLeft(AvlTree::Node *) {
+    return nullptr;
+}
+
+int AvlTree::getBalance(AvlTree::Node *) {
+    return 0;
+}
+
+AvlTree::Node *AvlTree::insertNode(AvlTree::Node *) {
+    return nullptr;
+}
+
+void AvlTree::preOrderTree(AvlTree::Node *) {
+
+}
--- Assignments/Assignment_04B/AvlTree.h
+++ Assignments/Assignment_04B/AvlTree.h
@@ -13,6 +13,9 @@ class AvlTree
 {
 
     public:
+
+    AvlTree(); //The ol' default constructor
+    ~AvlTree(); //The ol' default destructor
     //Our node what contains the data
     struct Node
     {
@@ -29,8 +32,10 @@ class AvlTree
     int getHeight(Node*); //Getting the height of the tree
     Node* createNode(int); //Creates a single node to be used in the insert
     Node* rorateRight(Node*); //Performs the rightward rotation
-
-
+    Node* rotateLeft(Node*); //Does a rotate to the devil's direction
+    int getBalance(Node*); //Gets the balance of the node
+    Node*insertNode(Node*);//This actually puts the new node into the tree and performs the balancing
+    void preOrderTree(Node*);//Peforms a pre-order traversal of the tree and displays what it finds
 
 
 
