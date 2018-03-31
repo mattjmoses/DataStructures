7d6918fabdad0d411db74848c656c7dbd34cfa8e
mattmoses <mattjmoses@gmail.com>
2018-03-31 00:38:07 -0300

    I have re-written this goddamn thing too many times.

--- Assignments/Assignment_04B/AvlTree.h
+++ Assignments/Assignment_04B/AvlTree.h
@@ -5,14 +5,15 @@
 #ifndef ASSIGNMENT_04B_AVLTREE_H
 #define ASSIGNMENT_04B_AVLTREE_H
 
-//Our node structure
 
 
+//Here now is the header for our AVL tree
+//Code referenced from GeeksForGeeks.com
 class AvlTree
 {
 
     public:
-
+    //Our node what contains the data
     struct Node
     {
         //Our data
@@ -24,14 +25,13 @@ class AvlTree
         int height;
     };
 
-    void deleteTree(Node*);//Deletes our tree
-    Node* search(Node,int);//Searches our tree
-    Node* findMin(Node*);//Finds the min node in the tree
-    Node* findMax(Node*);//Finds the max node in the tree
-    Node* insertNode(Node*, int); //Inserts a new node into the tree
-    Node* deleteNode(Node*, int); //Removes a node from the tree
-    void displayTree(Node*); //Displays the whole tree
-    int getNode(Node*);//And get's a single node *cough*
+    int max(int, int); //getting the max integer. This is subject to change
+    int getHeight(Node*); //Getting the height of the tree
+    Node* createNode(int); //Creates a single node to be used in the insert
+    Node* rorateRight(Node*); //Performs the rightward rotation
+
+
+
 
 
 };
