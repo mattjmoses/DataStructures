23732eb8613b054297eb7188377a572d9027e8dd
mattmoses <mattjmoses@gmail.com>
2018-03-31 19:45:31 -0300

    Getting the rotations to work.

--- Assignments/Assignment_04B/AvlTree.cpp
+++ Assignments/Assignment_04B/AvlTree.cpp
@@ -3,17 +3,20 @@
 //
 
 #include "AvlTree.h"
+#include <stdlib.h>
 
 AvlTree::AvlTree() = default;
 
 AvlTree::~AvlTree() = default;
 
+//Figuring out which integer is bigger
 int AvlTree::max(int a, int b)
 {
     //Is a larger than b? if so return a, otherwise return b
+    //Turnary operators :0
     return (a > b)? a : b;
 }
-
+//Getting the height of the tree. Takes in a node
 int AvlTree::getHeight(Node *node)
 {
     //Here we check if the node is actually there
@@ -28,12 +31,38 @@ int AvlTree::getHeight(Node *node)
         }
 
 }
+//Creating a brand new node -used in the insert function
+AvlTree::Node *AvlTree::createNode(int data)
+{
+    //Being fancy allocating a memory thing with the node.
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    //giving the node the data we want
+    newNode->data = data;
+    //Initializing our pointers kind of. They point to nothing
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+    //Creating a default height, This will change once the node is actually
+    //added to the tree
+    newNode->height = 1;
 
-AvlTree::Node *AvlTree::createNode(int) {
     return nullptr;
 }
 
-AvlTree::Node *AvlTree::rorateRight(AvlTree::Node *) {
+AvlTree::Node *AvlTree::rorateRight(Node *nodeY)
+{
+    //Node x and y are both root nodes
+    //Pointing to our pointers
+    Node *nodeX = nodeY->left;
+    //Is a subtree of X
+    Node* subTree = nodeX->right;
+
+    //So we rotate
+    nodeX->right = nodeY;
+    nodeY->left = subTree;
+
+    //Updating our node heights
+    nodeY->height = max(getHeight(nodeY->left),getHeight(nodeY->right)) + 1;
+    nodeX->height = max(getHeight(nodeX->left),getHeight(nodeX->right)) +1;
     return nullptr;
 }
 
