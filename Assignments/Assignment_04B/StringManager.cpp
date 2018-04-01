f9d601354c5f0764e12451da791e065cad86689b
matt <mattjmoses@gmail.com>
2018-04-01 15:02:33 -0300

    Alright. So the dictionary's reading in correctly. We just need to get the string stripper going so we can remove any special characters

---
+++ Assignments/Assignment_04B/StringManager.cpp
@@ -0,0 +1,15 @@
+//
+// Created by matt on 01/04/18.
+//
+
+#include "StringManager.h"
+
+StringManager::StringManager() = default;
+
+StringManager::~StringManager() = default;
+
+//This is going to strip out any special characters from our word so we can see if it's spelled correctly
+void StringManager::stringStripper(string word)
+{
+
+}
--- Assignments/Assignment_04B/main.cpp
+++ Assignments/Assignment_04B/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cstring>
+#include <algorithm>
 #include "Node.h"
 #include "AvlTree.h"
 
 using namespace std;
 int main()
 {
+    void stringStripper( string &str, char* toRemove ) {
+        for ( unsigned int i = 0; i < strlen(toRemove); ++i ) {
+            str.erase( remove(str.begin(), str.end(), toRemove[i]), str.end() );
+        }
+        cout << str << endl;
+    }
+
+
     Node* root = nullptr;
     AvlTree tree;
     ifstream file;
     file.open("dictionary.txt");
     string word;
+
+    //Feeding the dictionary into the AVL tree
     if(!file.is_open())
     {
         cout << "error happen :C"<< endl;
@@ -23,20 +35,17 @@ int main()
             root = tree.insertNode(root, word);
         }
     }
+    file.close();
 
 
 
 
 
 
-
-
-    tree.preOrderTree(root);
-
-
-
+    //tree.preOrderTree(root);
 
 
     return 0;
 
 }
+
